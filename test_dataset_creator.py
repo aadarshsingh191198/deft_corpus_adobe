aa37440c5dfbdc019f7d727005d2ac8ab59f9906
Aadarsh Singh <aadarshsingh191198@gmail.com>
2020-02-24 13:23:35 +0530

    Updated test data creation script

--- test_dataset_creator.py
+++ test_dataset_creator.py
@@ -2,8 +2,10 @@ import os
 import codecs
 import re
 import pandas as pd
+import shutil
 
-def generate_csv(dir, file):
+
+def subtask_1(dir, file):
     # subjects = ['history','pschology','history','sociology','physics','government','economics']
     # csv_file = codecs.open(file, 'w+','utf-8')
     # csv_file.write(','.join(['Subject','Sentence','Label\n'])) 
@@ -21,9 +23,36 @@ def generate_csv(dir, file):
     df.to_csv(file, index=False)                
     # csv_file.close()
 
+def submit_subtask1(dir1, dir2, filename,label_col):
+    i=0
+    test_csv = pd.read_csv(filename)
+    y = test_csv[label_col]
+    from_dir = dir1
+    to_dir = dir2
+    # y = [randint(0,1) for i in range(859)]
+    # from_dir = os.path.join('data','test_files','subtask_1')
+    # to_dir = os.path.join('data','test_files','submit_task_1')
+    for file in os.listdir(from_dir):
+        to_file = open(os.path.join(to_dir,file),'x',encoding='utf-8') #Creating and writing to a new file
+        from_file = open(os.path.join(from_dir,file),'r', encoding='utf-8')
+        sentences = from_file.readlines()
+        for sentence in sentences:
+            to_file.write(sentence.rstrip('\n')+'\t'+str(y[i])+'\n')
+            i+=1
+        to_file.close()
+        from_file.close()
 
 if __name__ == '__main__':
     # Test data for subtask 1
     test_path = os.path.join('data','test_files')
-    generate_csv(os.path.join(test_path, 'subtask_1'),'task1_test.csv')
+
+    # To generate test data
+    # subtask_1(os.path.join(test_path, 'subtask_1'),'task1_test.csv')
+
+    #To generate submission files
+    from_dir = os.path.join('data','test_files','subtask_1')
+    to_dir = os.path.join('data','test_files','submit_task_1')
+    # if 'submit_task_1' in os.listdir(test_path):
+    #     shutil.rmtree(os.path.join(test_path,'submit_task_1'))
+    submit_subtask1(from_dir, to_dir, 'submission_baseline.csv','PredictedClass')
 
