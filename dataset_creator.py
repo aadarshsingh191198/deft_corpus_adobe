62c42b139f2092b0c9db5a48f9f2299a5918cc84
Aadarsh Singh <aadarshsingh191198@gmail.com>
2020-02-27 10:52:01 +0530

    Fixed subject in the train and dev files

--- dataset_creator.py
+++ dataset_creator.py
@@ -11,7 +11,7 @@ def generate_csv(dir, file):
             x= myfile.readlines()
             for line in x:
                 try:
-                    csv_file.write(','.join([re.findall('[a-z]+',filename)[1]]+line.split('\t'))) 
+                    csv_file.write(','.join([filename.split('_')[3]]+line.split('\t'))) 
                     #Appending the subject using filename into the row
                 except:
                     print('Error: This should have been printed -> '+','.join(line.split('\t')))
